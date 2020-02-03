7b73bee627d72e71cb3f14dbb7a672f5affda876
Oskari Virtaoja <oskari@smartly.io>
2020-02-03 09:42:12 +0200

    Add entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
-#!/bin/sh -l
+#!/bin/bash
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -euo pipefail
+
+# Setup authentication for npm and mark the file as non-changed
+
+if [ -n "$AUTH_TOKEN_STRING" ]
+then
+    echo -e "$AUTH_TOKEN_STRING" >> .npmrc
+    git update-index --assume-unchanged .npmrc
+fi
+
+# The script is run as root so we need to allow npm to execute scripts as root.
+echo "unsafe-perm = true" >> ~/.npmrc
+
+# Setup SSH keys so we can push lerna commits and tags to master branch
+
+mkdir -p /root/.ssh
+ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
+echo "$GIT_DEPLOY_KEY" > /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+
+# Setup git
+
+git config user.email "$INPUT_EMAIL"
+git config user.name "$INPUT_USERNAME"
+
+git remote set-url origin git@github.com:$GITHUB_REPOSITORY.git
