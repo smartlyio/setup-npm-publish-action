15ec1f2b40500f80453cbb299a83c610d7d7976e
Simon Jagoe <simon.jagoe@smartly.io>
2021-01-19 15:03:43 +0200

    Test cleanup

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -10,8 +10,8 @@ import {
   getEnv,
   getSshPath,
   sshKeyscan,
-  setupNpmPublish
-  // cleanupNpmPublish
+  setupNpmPublish,
+  cleanupNpmPublish
 } from '../src/setup-npm-publish'
 
 jest.mock('@actions/core', () => ({
@@ -103,5 +103,23 @@ ${UNSAFE_PERM}
     expect(mockExec.mock.calls[2]).toEqual(['git', ['config', 'user.email', email]])
     expect(mockExec.mock.calls[3]).toEqual(['git', ['config', 'user.name', username]])
     expect(mockExec.mock.calls[4]).toEqual(['git', ['config', 'core.sshCommand', expect.stringContaining('UserKnownHostsFile')]])
+  })
+
+  test('cleanupNpmPublish', async () => {
+    const keyPath = path.join(homeTmpDir as string, '.ssh', 'id_rsa')
+    const hostsPath = path.join(homeTmpDir as string, '.ssh', 'known_hosts')
+    await cleanupNpmPublish()
+
+    const mockExec = mocked(exec)
+    expect(mockExec.mock.calls.length).toEqual(8)
+
+    expect(mockExec.mock.calls[0]).toEqual(['shred', ['-zuf', keyPath]])
+    expect(mockExec.mock.calls[1]).toEqual(['shred', ['-zuf', hostsPath]])
+    expect(mockExec.mock.calls[2]).toEqual(['shred', ['-zf', '.npmrc']])
+    expect(mockExec.mock.calls[3]).toEqual(['git', ['update-index', '--no-assume-unchanged', '.npmrc']])
+    expect(mockExec.mock.calls[4]).toEqual(['git', ['checkout', '.npmrc']])
+    expect(mockExec.mock.calls[5]).toEqual(['git', ['config', '--unset', 'user.email']])
+    expect(mockExec.mock.calls[6]).toEqual(['git', ['config', '--unset', 'user.name']])
+    expect(mockExec.mock.calls[7]).toEqual(['git', ['config', '--unset', 'core.sshCommand']])
   })
 })
