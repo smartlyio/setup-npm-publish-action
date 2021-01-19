2dd7f02b3f2449048a4ae6632733f018316c6fcd
Simon Jagoe <simon.jagoe@smartly.io>
2021-01-19 10:22:42 +0200

    Convert action to composite action

--- configure-npm-publish.bash
+++ configure-npm-publish.bash
@@ -2,11 +2,7 @@
 
 set -euo pipefail
 
-# Directory outside docker container corresponding to $HOME here
-EXT_HOME=/home/runner/work/_temp/_github_home
-
 # Validate input
-
 if [ -z "$GIT_DEPLOY_KEY" ]
 then
     echo "No GIT_DEPLOY_KEY environment variable set"
@@ -31,8 +27,8 @@ echo "$GIT_DEPLOY_KEY" > "$HOME/.ssh/id_rsa"
 chmod 400 "$HOME/.ssh/id_rsa"
 
 # Setup git
-git config user.email "$INPUT_EMAIL"
-git config user.name "$INPUT_USERNAME"
-git config core.sshCommand "ssh -i ${EXT_HOME}/.ssh/id_rsa -o UserKnownHostsFile=${EXT_HOME}/.ssh/known_hosts"
+git config user.email "$EMAIL"
+git config user.name "$USERNAME"
+git config core.sshCommand "ssh -i ${HOME}/.ssh/id_rsa -o UserKnownHostsFile=${HOME}/.ssh/known_hosts"
 
 git remote set-url origin "git@github.com:$GITHUB_REPOSITORY.git"
