60493c897c413767c6f878a62c3398c291bf3dfa
Simon Jagoe <simon.jagoe@smartly.io>
2021-09-15 10:03:51 +0300

    Fix code for new typescript

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function run(): Promise<void> {
       await cleanupNpmPublish()
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed((error as Error).message)
   }
 }
 
